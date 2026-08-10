d94fd42ad01ec958b417786d4593e8ec97d9f718
Philip B. Krogh <71797726+phibkro@users.noreply.github.com>
2026-08-10 23:38:26 +0200

    fix: initialize interception before sandbox start

--- packages/cloudflare/src/environment-worker.ts
+++ packages/cloudflare/src/environment-worker.ts
@@ -5,10 +5,11 @@ export { ContainerProxy } from "@cloudflare/sandbox";
 export class Sandbox extends CloudflareSandbox {
   override enableInternet = false;
   override interceptHttps = true;
+  override allowedHosts = ["*.r2.cloudflarestorage.com"];
   override entrypoint = [
     "/bin/sh",
     "-lc",
-    "cp /etc/cloudflare/certs/cloudflare-containers-ca.crt /usr/local/share/ca-certificates/cloudflare-containers-ca.crt && update-ca-certificates && exec /container-server/sandbox",
+    "if [ -f /etc/cloudflare/certs/cloudflare-containers-ca.crt ]; then cp /etc/cloudflare/certs/cloudflare-containers-ca.crt /usr/local/share/ca-certificates/ && update-ca-certificates; fi; exec /container-server/sandbox",
   ];
 }
 
